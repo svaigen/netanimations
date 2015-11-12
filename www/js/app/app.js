2dce70b0eaf09742502ad1c9fbb2e17a9013f073
svaigen <alisson.svg@gmail.com>
2015-11-12 10:10:22 -0200

    finalizacao da animacao de protocolos inter-as

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,5 +1,6 @@
 angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalprecht.translate',
-'netanimations.threewayhandshake', 'netanimations.sequencenumber', 'netanimations.purealoha', 'netanimations.hybridmodel', 'netanimations.intraasrouting'])
+'netanimations.threewayhandshake', 'netanimations.sequencenumber', 'netanimations.purealoha',
+ 'netanimations.hybridmodel', 'netanimations.intraasrouting','netanimations.interasrouting'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -102,6 +103,15 @@ angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalpr
     }
   })
 
+  .state('app.inter-as-routing',{
+    url: '/inter-as-routing',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/inter-as-routing.html',
+        controller: 'InterAsRoutingCtrl'
+      }
+    }
+  })
   ;
 
   $urlRouterProvider.otherwise('/app/animations');
@@ -168,9 +178,9 @@ angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalpr
     HYBRID_MODEL_PRESENTATION_1: 'The set protocols of network architecture can be understood like a layered model, where each one is responsible by a task group, supporting a well-marked service\'s group to the superior layer protocol.',
     HYBRID_MODEL_PRESENTATION_2: 'The hybrid model removes the layer\'s "excess" from ISO/OSI model and improves the TCP/IP model. It\'s composed by 5 layers: Physical, Data Link, Network, Transport and Application.',
     HYBRID_MODEL_PRESENTATION_3: 'Next, it will be presented an example of model operation, where a source client will send a message to a destiny server. It will be observed all encapsulation/decapsulation message process.',
-    HYBRID_MODEL_PRESENTATION_4: 'The first step of encapsulation, the message crossed the <b>Application Layer</b>, that provides an interface between user processes and network. Protocol examples of this layer are: HTTP, SMTP, FTP, POP3, SSH, DNS. ',
+    HYBRID_MODEL_PRESENTATION_4: 'The first step of encapsulation, the message crossed the <b>Application Layer</b>, that provides an interface between user processes and network. Protocol examples of this layer are: HTTP,  DHCP, SMTP, FTP, POP3, SSH, DNS. ',
     HYBRID_MODEL_PRESENTATION_5: 'The second step of encapsulation, the message crossed the <b>Transport Layer</b>, that divides data into segments and ensures an secure data transmission. Protocol examples of this layer are: TCP and UDP.',
-    HYBRID_MODEL_PRESENTATION_6: 'The third step of encapsulation, the message crossed the <b>Network Layer</b>, that divides the segments into packages (datagrams); determines route and defines logical addresses. Protocol examples of this layer are: IP, ARP, DHCP.',
+    HYBRID_MODEL_PRESENTATION_6: 'The third step of encapsulation, the message crossed the <b>Network Layer</b>, that divides the segments into packages (datagrams); determines route and defines logical addresses. Protocol examples of this layer are: IP, ARP.',
     HYBRID_MODEL_PRESENTATION_7: 'The fourth step of encapsulation, the message crossed the <b>Data Lynk Layer</b>, that divides the package into frames, controls the device acess and manages the transmission, detecting and fixing errors in Physical Layer. Protocol examples of this layer is Ethernet.',
     HYBRID_MODEL_PRESENTATION_8: 'Lastly, the message crossed the <b> Physical Layer</b>, that sends bits from one node to another and describes procedures and mechanical, electrical and functional features.',
     HYBRID_MODEL_PRESENTATION_9: 'After the encapsulation process have been completed, the <b>physical device</b> will transport the message across the network that don\'t know who is the message\'s final receiver. That way, it will be transported to the next found node.',
@@ -233,10 +243,10 @@ angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalpr
 
     PURE_ALOHA_TITLE: 'Protocolo Aloha Puro',
     PURE_ALOHA_DESC: 'Resolvendo problemas de alocação de canais na camada de enlace',
-    PURE_ALOHA_PRESENTATION_1: 'O protocolo Aloha puro é um protocolo de acesso aleatório, sem intervalo e totalmente descentralizado.',
+    PURE_ALOHA_PRESENTATION_1: 'O protocolo Aloha puro é um protocolo de acesso aleatório para enlaces sem fio, sem intervalo e totalmente descentralizado.',
     PURE_ALOHA_PRESENTATION_2: 'Quando um quadro chega pela primeira vez, o host imediatamente transmite o quadro inteiro ao canal broadcast.',
     PURE_ALOHA_PRESENTATION_3: 'Usaremos um exemplo para demonstrar o funcionamento do protocolo Aloha. Ex: O host A deseja enviar um quadro para o host C e o encaminha ao canal broadcast.',
-    PURE_ALOHA_PRESENTATION_4: 'Durante a transmissão, o transmissor é capaz de inferir se houve uma colisão ou não do quadro enviado.',
+    PURE_ALOHA_PRESENTATION_4: 'Durante a transmissão, o transmissor retransmite o quadro se houver uma colisão',
     PURE_ALOHA_PRESENTATION_5: 'Agora, suponha que o host B também deseja enviar um quadro para o host C e o encaminha ao canal broadcast no mesmo momento em que o quadro enviado pelo host A é transmitido, ocasionando assim uma colisão.',
     PURE_ALOHA_PRESENTATION_6: 'Neste caso o host A retransmite imediatamente o quadro através do canal broadcast.',
     PURE_ALOHA_PRESENTATION_7: 'Considere p uma probabilidade, ou seja, um número entre 0 e 1.',
@@ -248,9 +258,9 @@ angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalpr
     HYBRID_MODEL_PRESENTATION_1: 'O conjunto de protocolos da arquitetura de redes pode ser visto como um modelo de camadas, onde cada uma é responsável por um grupo de tarefas, fornecendo assim um conjunto de serviços bem definidos para o protocolo da camada superior.',
     HYBRID_MODEL_PRESENTATION_2: 'O modelo híbrido retira o "excesso" de camadas do modelo ISO/OSI e melhora o modelo TCP/IP. É composto por 5 camadas: Fisica, Enlace, Rede, Transporte e Aplicação. ',
     HYBRID_MODEL_PRESENTATION_3: 'A seguir, será apresentado um exemplo de funcionamento do modelo, onde um cliente de origem enviará uma mensagem para um servidor de destino. Será observado todo o processo de encapsulamento e desencapsulamento da mensagem.',
-    HYBRID_MODEL_PRESENTATION_4: 'No primeiro passo de encapsulamento, a mensagem passa pela camada de <b>aplicação</b>, que fornece a interface entre os processos do usuário e a rede. Exemplos de protocolos dessa camada são: HTTP, SMTP, FTP, POP3, SSH, DNS.',
+    HYBRID_MODEL_PRESENTATION_4: 'No primeiro passo de encapsulamento, a mensagem passa pela camada de <b>aplicação</b>, que fornece a interface entre os processos do usuário e a rede. Exemplos de protocolos dessa camada são: HTTP, DHCP, SMTP, FTP, POP3, SSH, DNS.',
     HYBRID_MODEL_PRESENTATION_5: 'No segundo passo de encapsulamento, a mensagem passa pela camada de <b>transporte</b>, que divide os dados em segmentos e assegura a transmissão segura dos dados. Exemplos de protocolos dessa camada são: TCP, UDP.',
-    HYBRID_MODEL_PRESENTATION_6: 'No terceiro passo de encapsulamento, a mensagem passa pela camada de <b>rede</b>, que divide os segmentos em pacotes (datagramas); determina a rota e define endereços lógicos. Exemplos de protocolos dessa camada são: IP, ARP, DHCP.',
+    HYBRID_MODEL_PRESENTATION_6: 'No terceiro passo de encapsulamento, a mensagem passa pela camada de <b>rede</b>, que divide os segmentos em pacotes (datagramas); determina a rota e define endereços lógicos. Exemplos de protocolos dessa camada são: IP, ARP.',
     HYBRID_MODEL_PRESENTATION_7: 'No quarto passo de encapsulamento, a mensagem passa pela camada de <b>enlace</b>, que separa o pacote em quadros (frames), controla o acesso ao meio e gerencia a transmissão, detectando e corrigindo erros na camada física. Um exemplo de protocolo dessa camada é o Ethernet.',
     HYBRID_MODEL_PRESENTATION_8: 'Por fim, a mensagem passa pela camada <b>física</b>, que transmite os <i>bits</i> de um ponto a outro e descreve os procedimentos e as características mecanicas, elétricas e funcionais.',
     HYBRID_MODEL_PRESENTATION_9: 'Após as camadas completarem o processo de encapsulamento, o <b> meio físico </b> transportará a mensagem pela rede, que não sabe quem é o destinatário final da mensagem. Dessa maneira, ela será transportada para a próxima máquina encontrada.',
@@ -274,14 +284,33 @@ angular.module('netanimations', ['ionic', 'netanimations.controllers', 'pascalpr
     INTRA_AS_PRESENTATION_4: 'O RIP é um protocolo de vetor de distâncias que usa <b>contagem de saltos como métrica de custo</b>, assim, cada enlace tem um custo 1. O custo máximo de um caminho é limitado a 15.',
     INTRA_AS_PRESENTATION_5: 'Tabelas de roteamento são trocadas entre vizinhos a cada 30 segundos aproximadamente, <b>usando mensagem de resposta RIP.</b>',
     INTRA_AS_PRESENTATION_6: 'Essas mensagens são conhecidas como <b>anúncios RIP</b>, contendo uma lista de até 25 sub-redes de destino dentro do AS, bem como as distâncias entre o remetente e cada uma dessas sub-redes.',
-    INTRA_AS_PRESENTATION_7: 'Cada roteador mantém uma tabela RIP denominada <b>tabela de roteamento</b>, que contém o vetor de distâncias.',
+    INTRA_AS_PRESENTATION_7: 'Cada roteador mantém uma tabela RIP denominada <b>tabela e roteamento</b>, que contém o vetor de distâncias.',
     INTRA_AS_PRESENTATION_8: 'Suponha que o roteador <b>1b</b> deseja alcançar a rede <b>X</b>. Ele pode chegar nela por meio de 1a ou 1c, conforme mostra a animação.',
     INTRA_AS_PRESENTATION_9: 'Para que isso ocorra, <b>1b</b> precisa saber dessa informação. A seguir é apresentada a tabela desse roteador, que a princípio não possui informações referentes ao roteador 1c.',
     INTRA_AS_PRESENTATION_10: 'Agora, observe na sequência a tabela de roteamento de <b>1c</b>. Procure notar que, para esse roteador, alcançar a sub-rede <b>X</b> são necessários apenas 2 saltos.',
     INTRA_AS_PRESENTATION_11: 'Suponha que 30 segundos mais tarde, <b> 1b </b> recebe de <b>1c</b> um anúncio que indica que a sub-rede <b>x</b> está apenas 2 saltos do roteador <b>1c</b>. Observe as alterações na tabela de <b>1b</b>.',
     INTRA_AS_PRESENTATION_12: 'Dessa maneira, quando <b>1b</b> necessitar se comunicar com a sub-rede <b>X</b>, o caminho é o apresentado a seguir.',
-    INTRA_AS_PRESENTATION_13: 'Caso <b>1b</b> envie dados para a sub-rede <b>X</b>, o caminho realizado é o exibido a seguir.'
+    INTRA_AS_PRESENTATION_13: 'Caso <b>1b</b> envie dados para a sub-rede <b>X</b>, o caminho realizado é o exibido a seguir.',
 
+    INTER_AS: 'Protocolos Inter-AS',
+    INTER_AS_TITLE: 'Protocolos Inter-AS: i-BGP e e-BGP',
+    INTER_AS_DESC: 'Apresentação de conceitos e procedimentos dos protocolos de roteamento hierárquico inter-AS.',
+    INTER_AS_PRESENTATION_1: 'Para resolver problemas de escala e autonomia administrativa do roteamento na internet, realiza-se o agrupamento de roteadores em sistemas autônomos (autonomous systems - AS).',
+    INTER_AS_PRESENTATION_2: 'Cada AS é composto de um grupo de roteadores sob o <b>mesmo controle administrativo</b>, que rodam o <b>mesmo protocolo de roteamento</b> e dispõem de informações sobre cada um dos outros.',
+    INTER_AS_PRESENTATION_3: 'A versão 4 do <b>Protocolo de Roteador de Borda</b> (Border Gateway Protocol - BGP) é o padrão para roteamento entre sistemas autônomos na Internet.',
+    INTER_AS_PRESENTATION_4: 'O BGP provê a cada AS meio de: obter de ASs vizinhos informações de atingibilidade de sub-redes; propagar essas informação aos roteadores internos ao AS; determinar rotas "boas" para sub-redes com base na informação de atingibilidade e na política do AS',
+    INTER_AS_PRESENTATION_5: 'Nessa animação, temos como exemplo os sistemas autônomos <b>AS1</b> e <b>AS2</b>, cada qual com seu próprio protocolo de roteamento, ou seja, eles podem ser diferentes em cada AS.',
+    INTER_AS_PRESENTATION_6: '<b>Roteadores de borda</b> são roteadores que transmitem pacotes a destinos externos ao seu AS. Nesse exemplo, temos como roteadores de borda: <b>1a</b> em <b>AS1</b>, que possui uma "ligação" com AS2; e em AS2 temos <b>2a</b>, que possui ligação com AS1, e <b>2d</b>, que possui ligação com outro AS, não especificado na animação.',
+    INTER_AS_PRESENTATION_7: 'No quadro de animação a seguir, os roteadores de borda são destacados na cor verde, e os caminhos entre ASs são destacados na cor laranja.',
+    INTER_AS_PRESENTATION_8: 'Suponha que o roteador <b>1c</b> necessite transmitir um pacote para um destino fora do seu AS. Para isso, ele transmite o pacote conforme foi especificado pela tabela de repasse, configurada pelo próprio protocolo de roteamento <b>INTRA-AS</b> de AS1.',
+    INTER_AS_PRESENTATION_9: 'Como há apenas <b> um roteador de borda </b> em AS1, o problema de saber para quem transmitir é trivial!',
+    INTER_AS_PRESENTATION_10: 'Agora, suponha que <b>2c</b> necessite transmitir para fora de AS2. Sabemos que o pacote deve ser repassado para <b>2a</b> ou <b>2d</b>, que são os roteadores de borda. Mas para qual?',
+    INTER_AS_PRESENTATION_11: 'Para isso, AS2 precisa saber <b>quais destinos podem ser alcançados pelos ASs vizinhos</b>, e propagar essa informação para todos os seus roteadores. Essas tarefas são gerenciadas pelo protocolo <b>inter-as BGP</b>.',
+    INTER_AS_PRESENTATION_12: 'Isso faz com que os roteadores <b>troquem informações entre si</b> por meio de <b>conexões TCP semipermanentes</b> para cada par de enlace que conecte roteadores entre dois ASs diferentes. Essas conexões, juntamente com todas as mensagens BGP enviadas pela conexão, é denominada <b>Sessão BGP</b>.',
+    INTER_AS_PRESENTATION_13: 'As sessões BGP são classificadas como <b>externas (eBGP)</b>, quando abrange dois ASs diferentes, ou <b>internas (iBGP)</b>, quando abrange dois roteadores no mesmo AS. A seguir são apresentadas no exemplo as sessões eBGP em laranja e iBGP em azul.',
+    INTER_AS_PRESENTATION_14: 'O BGP permite que cada AS conheça <b>quais destinos podem ser alcançados</b> via seus ASs vizinhos. No exemplo, usando então sessões <b>eBGP</b> entre <b>1a</b> e <b>2a</b>, AS2 envia <b>informações de atingibilidade</b> de prefixos para AS1, e vice-versa.',
+    INTER_AS_PRESENTATION_15: 'Quando um roteador de borda recebe prefixos conhecidos pelo BGP, ele usa suas sessões <b>iBGP</b> para distribuir os prefixos aos outros roteadores no AS. No exemplo da animação, caso o roteador <b>1a</b> receba informações de <b>2a</b>, ele repassará a <b>1b</b> e <b>1c</b>.',
+    INTER_AS_PRESENTATION_16: 'O BGP ainda possui outros atributos importantes, como o <b>AS-PATH</b> e o <b>NEXT-HOP</b>. Para conhecer mais sobre esses atributos e ver essa animação mais detalhada, visite o repositório online das animações, pelo endereço: <b>http://din.uem.br/~animanet/pages/hierarchicalRouting</b>.'
   });
 
   $translateProvider.preferredLanguage('pt-br');
---
+++ www/js/app/inter-as-routing.js
@@ -0,0 +1,352 @@
+angular.module('netanimations.interasrouting', [])
+.controller('InterAsRoutingCtrl', function($scope, $ionicPopup, $translate) {
+  TweenLite.defaultEase = Power1.easeInOut;
+  var tl = new TimelineMax();
+  $scope.end = false;
+  var segment1 = "#segment1";
+  var segment2 = "#segment2";
+  var bg2 = "../img/inter-as-routing/inter-routing-2.svg";
+  var patternHeight = 595;
+  var patternWidth = 350;
+  var width = window.innerWidth;
+  var height = window.innerHeight - 44; // 44 é a altura do header, que deve ser desconsiderado
+  var boxWidth = transform(patternHeight, patternWidth, height, width, 20, 'x', true);
+  var boxHeight = transform(patternHeight, patternWidth, height, width, 20, 'y', true);
+  var transmission1cTo1aTop = transform(patternHeight, patternWidth, height, width, 70, 'y', false);
+  var transmission1cTo1aLeft = transform(patternHeight, patternWidth, height, width, 262, 'x', false);
+  var crossingVerticalPipe1 = transform(patternHeight, patternWidth, height, width, 50, 'y', false);
+  var crossingHorizontalPipe1 = transform(patternHeight, patternWidth, height, width, 60, 'x', false);
+  var transmission1aTo2aTop = transform(patternHeight, patternWidth, height, width, 150, 'y', false);
+  var transmission1aTo2aLeft = transform(patternHeight, patternWidth, height, width, 162, 'x', false);
+  var crossingAS1ToAS2 = transform(patternHeight, patternWidth, height, width, 100, 'y', false);
+  var transmission1aTo1cTop = transform(patternHeight, patternWidth, height, width, 120, 'y', false);
+  var transmission1aTo1cLeft = transform(patternHeight, patternWidth, height, width, 186, 'x', false);
+  var transmission1aTo1bTop = transform(patternHeight, patternWidth, height, width, 120, 'y', false);
+  var transmission1aTo1bLeft = transform(patternHeight, patternWidth, height, width, 139, 'x', false);
+
+  $scope.restart = function () {
+    tl.seek(0);
+    $scope.end = false;
+  };
+
+  //initialize segment1 and segment2's properties
+  tl.set(segment1,{width:boxWidth});
+  tl.set(segment1,{height: boxHeight});
+  tl.set(segment2,{width:boxWidth});
+  tl.set(segment2,{height: boxHeight});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_1']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_1
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_2']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_2
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_3']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_3
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_4']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_4
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_5']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_5
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 2, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_6']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_6
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_7']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_7
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  //change background to bg2
+  tl.to('.bg',1,{autoAlpha:0});
+  tl.set('.bg',{className:"+=inter-as-bg2",opacity:1});
+  tl.to('.bg',2,{autoAlpha:1});
+  tl.set('.bg',{className:"-=inter-as-bg1"});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_8']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_8
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  //sending a package from 1c in AS1 to AS2
+  tl.set(segment1,{top:transmission1cTo1aTop});
+  tl.set(segment1,{left:transmission1cTo1aLeft})
+  tl.to(segment1,1,{className: "-=hide"});
+  tl.to(segment1,1,{top: "+="+crossingVerticalPipe1});
+  tl.to(segment1,1,{left: "-="+crossingHorizontalPipe1});
+  tl.to(segment1,0.5,{autoAlpha:0});
+  tl.set(segment1,{top:transmission1aTo2aTop});
+  tl.set(segment1,{left:transmission1aTo2aLeft})
+  tl.to(segment1,0.5,{autoAlpha:1});
+  tl.to(segment1,1.5,{top:"+="+crossingAS1ToAS2});
+  tl.to(segment1,0.5,{autoAlpha:0});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_9']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_9
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_10']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_10
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  //change background to bg3 and return to bg2
+  tl.to('.bg',0.5,{autoAlpha:0});
+  tl.set('.bg',{className:"+=inter-as-bg3"});
+  tl.to('.bg',2,{autoAlpha:1});
+  tl.to('.bg',0.5,{autoAlpha:0});
+  tl.set('.bg',{className:"-=inter-as-bg3"});
+  tl.to('.bg',0.5,{autoAlpha:1});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_11']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_11
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_12']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_12
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_13']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_13
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+  //change background to bg4
+  tl.to('.bg',0.5,{autoAlpha:0});
+  tl.set('.bg',{className:"+=inter-as-bg4"});
+  tl.to('.bg',2,{autoAlpha:1});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_14']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_14
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  //eBGP animation
+  tl.set(segment1,{className: "-=hide"});
+  tl.set(segment1,{autoAlpha:0});
+  tl.to(segment1,0.5,{autoAlpha:1});
+  tl.to(segment1,1.5,{top:"-="+crossingAS1ToAS2});
+  tl.to(segment1,1,{autoAlpha:0});
+  tl.to(segment1,2,{autoAlpha:1});
+  tl.to(segment1,1.5,{top:"+="+crossingAS1ToAS2});
+  tl.to(segment1,0.5,{autoAlpha:0});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_15']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_15
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  //iBGP animation
+  tl.set(segment1,{className: "-=hide"});
+  tl.set(segment1,{autoAlpha:0});
+  tl.to(segment1,0.5,{autoAlpha:1});
+  tl.to(segment1,1.5,{top:"-="+crossingAS1ToAS2});
+  tl.to(segment1,1,{autoAlpha:0});
+  tl.set(segment1,{top:transmission1aTo1bTop});
+  tl.set(segment1,{left:transmission1aTo1bLeft});
+  tl.set(segment2,{top:transmission1aTo1cTop});
+  tl.set(segment2,{left:transmission1aTo1cLeft});
+  tl.set(segment2,{className: "-=hide"});
+  tl.set(segment2,{autoAlpha:0});
+  tl.to(segment1,0.5,{autoAlpha:1});
+  tl.to(segment2,0.5,{autoAlpha:1, delay: -0.5});
+  tl.to(segment1,1,{left: "-="+crossingHorizontalPipe1});
+  tl.to(segment2,1,{left: "+="+crossingHorizontalPipe1, delay: -1});
+  tl.to(segment1,1,{top: "-="+crossingVerticalPipe1});
+  tl.to(segment2,1,{top: "-="+crossingVerticalPipe1, delay: -1});
+  tl.to(segment1,1,{autoAlpha:0});
+  tl.to(segment2,1,{autoAlpha:0,delay:-1});
+
+  tl.call(function() {
+    tl.pause();
+
+    $translate(['INFO', 'INTER_AS_PRESENTATION_16']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.INFO,
+        template: translations.INTER_AS_PRESENTATION_16
+      }).then(function() {
+        tl.resume();
+      });
+    });
+  });
+  tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
+
+  tl.call(function() {
+    $translate(['END', 'ANIMATION_END']).then(function(translations) {
+      $ionicPopup.alert({
+        title: translations.END,
+        template: translations.ANIMATION_END
+      });
+    });
+  });
+
+  $scope.end = true;
+
+});
+
+function transform(patternHeight, patternWidth, realHeight, realWidth, value, coordinate, isSize){
+  var patternAspectRatio = patternWidth/patternHeight;
+  if (coordinate == 'x'){
+    var consideredWidth = realHeight * patternAspectRatio;
+    var newValue = (value * consideredWidth) / patternWidth;
+    if(consideredWidth < realWidth && !isSize){
+      return (((realWidth - consideredWidth) / 2) + newValue);
+    } else{
+      return newValue;
+    }
+  } else { //coordinate == 'y'
+  return (value * (realHeight/patternHeight));
+  }
+}
